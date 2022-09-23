1e1d4433ed5dbc28bb7c4f5cb24e01ae8414faab
Thiago <thiago.ralves02@gmail.com>
2022-09-23 14:49:59 -0300

    feat: estilo do botao

---
+++ loja-filmes-dot/src/components/Button/style.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const ButtonContainer = styled.button`
+    width:200px;
+    height:40px;
+    border-radius:5px ;
+    background-color:#6558F5;
+    border:none ;
+    color: #ffff;
+    cursor: pointer;
+    font-size:17px;
+    font-weight:bold;
+`
