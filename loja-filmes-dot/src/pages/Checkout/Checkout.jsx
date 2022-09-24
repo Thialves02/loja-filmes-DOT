53a347967853ba43d706f1302920ec0dc54296ee
Thiago <thiago.ralves02@gmail.com>
2022-09-24 19:37:32 -0300

    feat: pagina de checkout responsiva

--- loja-filmes-dot/src/pages/Checkout/Checkout.jsx
+++ loja-filmes-dot/src/pages/Checkout/Checkout.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { CheckoutContainer } from './style'
+import DadosUsuario from '../../components/DadosUsuario/DadosUsuario';
+import FinalizarCompra from '../../components/FinalizarCompra/FinalizarCompra';
+import Modal from '../../components/Modal/Modal';
 
 export default function Checkout() {
     return (
-        <div>Checkout</div>
+        <CheckoutContainer>
+            <DadosUsuario />
+            <FinalizarCompra />
+            <Modal />
+        </CheckoutContainer>
     )
 }
---
+++ loja-filmes-dot/src/pages/Checkout/style.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+import { media } from "../../assets/styles/media"
+
+export const CheckoutContainer = styled.div`
+    margin-top:100px;
+    display:flex ;
+    /* width:80%; */
+    flex-wrap:wrap;
+    justify-content:space-around;
+    align-items:center;
+
+    ${media('desktop')}{
+        flex-direction:column;
+    }
+`
