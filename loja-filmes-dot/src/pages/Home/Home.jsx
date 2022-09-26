6b78908bdcda7b81f8dc9ffcaa1a0bc8a5aae14d
Thiago <thiago.ralves02@gmail.com>
2022-09-26 16:34:45 -0300

    feat: componentes da home

--- loja-filmes-dot/src/pages/Home/Home.jsx
+++ loja-filmes-dot/src/pages/Home/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Filmes from '../../components/Filmes/Filmes'
+import { HomeContainer } from './style'
 
 export default function Home() {
     return (
-        <div>Home</div>
+        <HomeContainer>
+            <Filmes />
+        </HomeContainer>
     )
 }
---
+++ loja-filmes-dot/src/pages/Home/style.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+export const HomeContainer = styled.main`
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    margin:100px 0 ;
+`
