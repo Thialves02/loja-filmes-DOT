a9f64ea99972c95e6c18e5b7c1926d33693f215f
Thiago <thiago.ralves02@gmail.com>
2022-09-26 12:59:44 -0300

    feat: componente recebendo ...rest

--- loja-filmes-dot/src/components/ValorTotal/ValorTotal.jsx
+++ loja-filmes-dot/src/components/ValorTotal/ValorTotal.jsx
@@ -1,12 +1,12 @@
 import React, { useContext } from 'react'
 import { Context } from '../../context/CtxApp'
 import Button from '../Button/Button'
-import { PrecoContainer } from './style'
+import { PrecoContainer, ValorTotalContainer } from './style'
 
-export default function ValorTotal({ name, label, type, funcao }) {
+export default function ValorTotal({ name, label, type, funcao, disabled, ...rest }) {
     const { precoTotal } = useContext(Context)
     return (
-        <div>
+        <ValorTotalContainer {...rest}>
             <PrecoContainer>
                 <p>Total:</p>
                 <h2>R$ {precoTotal},00</h2>
@@ -17,7 +17,8 @@ export default function ValorTotal({ name, label, type, funcao }) {
                 type={type}
                 onClick={funcao}
                 className={'grande'}
+                disabled={disabled}
             />
-        </div>
+        </ValorTotalContainer>
     )
 }
--- loja-filmes-dot/src/components/ValorTotal/style.jsx
+++ loja-filmes-dot/src/components/ValorTotal/style.jsx
@@ -1,8 +1,22 @@
 import styled from "styled-components"
 
+export const ValorTotalContainer = styled.div`
+    &.menuLateral {
+        position: absolute;
+        width: 92%;
+        bottom: 100px;
+    }
+`
+
 export const PrecoContainer = styled.div`
     display:flex;
     justify-content:space-between;
     align-items:center;
     margin:50px 0 ;
+
+    &.menuLateral {
+        position: absolute;
+        width: 92%;
+        bottom: 100px;
+    }
 `
