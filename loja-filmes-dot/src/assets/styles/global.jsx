c26204887e53e78912a2e7fd544c010808de43b1
Thiago <thiago.ralves02@gmail.com>
2022-09-26 16:20:06 -0300

    feat: cor para textos

--- loja-filmes-dot/src/assets/styles/global.jsx
+++ loja-filmes-dot/src/assets/styles/global.jsx
@@ -5,9 +5,10 @@ export default createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        font-family: 'Source Sans Pro', sans-serif;
     }
 
-    html,body {
-        height: 100%;
+    h1,h2,p{
+        color:#293845 ;
     }
 `
