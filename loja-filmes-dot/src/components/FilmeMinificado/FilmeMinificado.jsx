38e4c776feaa216c0af2a3df6d7fc1abb5be8ef0
Thiago <thiago.ralves02@gmail.com>
2022-09-26 16:32:09 -0300

    feat: quantidade dinamica de itens

--- loja-filmes-dot/src/components/FilmeMinificado/FilmeMinificado.jsx
+++ loja-filmes-dot/src/components/FilmeMinificado/FilmeMinificado.jsx
@@ -1,53 +1,75 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash, faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import { FilmeMinContainer } from './style'
 import { Context } from '../../context/CtxApp'
 
-export default function FilmeMinificado({ capa, titulo, preco, menuFav, index }) {
+export default function FilmeMinificado({ capa, titulo, preco, menuFav, quantidade, index, ...rest }) {
     const { filmeCarrinho, filmeFav, setFilmeFav, setFilmeCarrinho } = useContext(Context)
 
+    //Remove o filme selecionado
     const removeFilme = (index) => {
-        console.log('clicado')
         var filmes
-        menuFav == false ? filmes = filmeCarrinho : filmes = filmeFav
+        menuFav === false ? filmes = filmeCarrinho : filmes = filmeFav
         filmes.splice(index, 1);
-        menuFav == false ? setFilmeCarrinho(filmes) : setFilmeFav(filmes)
+        menuFav === false ? setFilmeCarrinho(filmes) : setFilmeFav(filmes)
     };
 
-    const adicionaFilme = (menuFav) => {
+    const adicionaFilme = () => {
         let filmes = []
 
-        if (menuFav == false) {
-            if (filmeFav) {
-                filmes = filmeFav
-            }
-        } else {
-            if (filmeCarrinho) {
-                filmes = filmeCarrinho
-            }
+        //Preenche variavel com valor do LocalStorage caso ele exista
+        if (filmeCarrinho) {
+            filmes = filmeCarrinho
         }
 
-        filmes.push({
-            'titulo': titulo,
-            'capa': capa,
-            'preco': preco,
-            'id': index
-        })
-
-        return menuFav == false ? setFilmeFav(filmes) : setFilmeCarrinho(filmes)
+        //Adiciona um filme no LocalStorage do carrinho
+        if (filmeArmazenado(filmes) === undefined) {
+            filmes.push({
+                'titulo': titulo,
+                'capa': capa,
+                'preco': preco,
+                'id': index,
+                'quantidade': 1
+            })
+            setFilmeCarrinho(filmes)
+        }
+        //Valida se o filme ja estava no carrinho, entao soma 1 na quantidade
+        else if (filmeArmazenado(filmes) === true) {
+            filmes?.forEach(filme => {
+                if (filme.titulo === titulo) {
+                    filme.quantidade = filme.quantidade + 1
+                    filme.preco += filme.preco
+                }
+            })
+            setFilmeCarrinho(filmes)
+        }
     }
 
-    useEffect(() => { }, [filmeCarrinho]);
+    //Função que valida se o filme está no LocalStorage do tipo selecionado, "favorito" ou "carrinho"
+    const filmeArmazenado = (tipo) => {
+        var armazenado
+        tipo?.forEach(filme => {
+            if (filme.titulo === titulo) {
+                return armazenado = true
+            }
+        })
+        return armazenado
+    }
+
     return (
-        <FilmeMinContainer>
+        <FilmeMinContainer {...rest} key={index}>
             <img src={`https://image.tmdb.org/t/p/w300${capa}`} alt={titulo} />
             <h2>{titulo}</h2>
-            <p>1</p>
+            {
+                menuFav === false && (
+                    <p>{quantidade}</p>
+                )
+            }
             <p>R$ {preco},00</p>
             {
-                menuFav == true && (
-                    <FontAwesomeIcon icon={faCartShopping} onClick={() => adicionaFilme(menuFav)} />
+                menuFav === true && (
+                    <FontAwesomeIcon icon={faCartShopping} className={'carrinho'} onClick={() => adicionaFilme()} />
                 )
             }
             <FontAwesomeIcon icon={faTrash} onClick={() => removeFilme(index)} />
--- loja-filmes-dot/src/components/FilmeMinificado/style.jsx
+++ loja-filmes-dot/src/components/FilmeMinificado/style.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components"
-import { media } from "../../assets/styles/media"
 
 export const FilmeMinContainer = styled.div`
     display:flex ;
@@ -9,13 +8,40 @@ export const FilmeMinContainer = styled.div`
     overflow:hidden;
     padding:0 10px ;
 
+    &.grande{
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        grid-template-rows: 1fr;
+        text-align:center;
+        align-content:center ;
+        padding:10px ;
+        border-bottom: 2px solid #9EADBA;
+        margin:0 ;
+
+        :last-child{
+            border:none ;
+        }
+
+        img{
+            width:70px;
+            height:70px;
+        }
+
+        svg {
+            width:100% ;
+            cursor:pointer;
+            height:22px;
+        }
+    }
+
     h2{
-        padding:5px 0;
+        padding:5px 10px;
         font-size:18px;
         text-align: center;
         max-width:100px;
         min-width:100px;
         max-height:50px;
+        overflow:hidden;
     }
 
     img{
@@ -26,5 +52,9 @@ export const FilmeMinContainer = styled.div`
     svg {
         cursor:pointer;
         height:20px;
+
+        &.carrinho{
+            color:#1AAE9F
+        }
     }
 `
