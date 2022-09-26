0b592e699d1d9ba2c9a4ad51b7d566731ac4b448
Thiago <thiago.ralves02@gmail.com>
2022-09-26 16:38:18 -0300

    comentarios adicionados

--- loja-filmes-dot/src/components/Filmes/Filmes.jsx
+++ loja-filmes-dot/src/components/Filmes/Filmes.jsx
@@ -1,12 +1,9 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Api } from '../../api/Api'
+import React, { useContext, useEffect } from 'react'
 import CardFilme from '../CardFilme/CardFilme'
 import { FilmesContainer } from './style'
 import InfiniteScroll from "react-infinite-scroll-component";
 import { Context } from '../../context/CtxApp';
 import axios from 'axios';
-import { SemResultadoContainer } from '../BuscaSemResultado/style';
-import BuscaSemResultado from '../BuscaSemResultado/BuscaSemResultado';
 import Loading from '../Loading/Loading';
 import MensagemInformativa from '../BuscaSemResultado/BuscaSemResultado';
 
@@ -15,34 +12,39 @@ export default function Filmes() {
 
     useEffect(() => {
         filmesPopulares()
-    }, [query == ''])
+    }, [query === ''])
 
     useEffect(() => {
 
     }, [filmes])
 
+    //Função para buscar os filmes populares da primeira página
     const filmesPopulares = async () => {
         const response = await axios.get(`${defaultURL}movie/popular?api_key=${API_KEY}&language=pt-BR&page=1`)
         setFilmes(response.data.results)
     }
 
+    //Função para buscar os filmes populares de acordo com a pagina atual
     const paginacaoFilmesPopulares = async () => {
         const response = await axios.get(`${defaultURL}movie/popular?api_key=${API_KEY}&language=pt-BR&page=${pagina}`)
         return response.data.results
     }
 
+    //Função para buscar os filmes pesquisados de acordo com a paginacao atual
     const paginacaoFilmesPesquisados = async () => {
         const response = await axios.get(`${defaultURL}search/movie?api_key=${API_KEY}&language=pt-BR&query='${query}'&page=${pagina}`)
         return response.data.results
     }
 
+    //Atualiza o conteudo da pagina adicionando novos filmes vindo das requisições
     const atualizaPaginacao = async () => {
         setPagina(previusState => previusState + 1);
-        const novosFilmes = query == '' ? await paginacaoFilmesPopulares() : await paginacaoFilmesPesquisados()
+        const novosFilmes = query === '' ? await paginacaoFilmesPopulares() : await paginacaoFilmesPesquisados()
         setHasMore(novosFilmes.length)
         setFilmes([...filmes, ...novosFilmes]);
     };
 
+    //Verifica se existem mais páginas
     const existeMaisPaginas = () => {
         return hasMore === 0 ? false : true
     }
@@ -62,6 +64,7 @@ export default function Filmes() {
             >
                 {filmes?.map((filme, index) => (
                     <CardFilme
+                        key={index}
                         index={index}
                         titulo={filme.title}
                         nota={filme.vote_average}
