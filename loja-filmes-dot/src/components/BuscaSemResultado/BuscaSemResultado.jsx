f92775e8c416d6dc27b606204ba2f83b0e8e4b43
Thiago <thiago.ralves02@gmail.com>
2022-09-25 21:04:59 -0300

    feat: Componente para buscas que nao retornam nada

---
+++ loja-filmes-dot/src/components/BuscaSemResultado/BuscaSemResultado.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SemResultadoContainer } from './style'
+
+export default function BuscaSemResultado() {
+    return (
+        <SemResultadoContainer>
+            <p>A busca não retornou mais resultados!</p>
+        </SemResultadoContainer>
+    )
+}
---
+++ loja-filmes-dot/src/components/BuscaSemResultado/style.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export const SemResultadoContainer = styled.div`
+    display:flex ;
+    align-items:center;
+    justify-content:center;
+    width:100vw;
+    height:100px ;
+
+    p {
+        font-size:25px;
+        font-weight:bold;
+    }
+`
